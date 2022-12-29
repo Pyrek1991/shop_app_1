0fbc437eaca3d0244ff08363605e8d952dfbd604
Piotr <piot.domzalski91@gmail.com>
2022-12-29 19:56:48 +0100

    creation of sub-folders to make it clearer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app_1/categories_screen.dart';
-import 'package:shop_app_1/category_meals_screen.dart';
+import 'package:shop_app_1/screens/categories_screen.dart';
+import 'package:shop_app_1/screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app_1/category_meals_screen.dart';
+import 'package:shop_app_1/screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
