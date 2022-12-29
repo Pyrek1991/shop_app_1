8a1509f678a34b13b8da2a3c59aac4f32d5d85f0
Piotr <piot.domzalski91@gmail.com>
2022-12-29 20:19:48 +0100

    setting the meal_item return

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app_1/widgets/meal_item.dart';
 import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
@@ -25,8 +26,14 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
-        }, itemCount: categoryMeals.length,
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
