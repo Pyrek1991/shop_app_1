840ef24de911a3115536c4ca40d13df958ce8df3
Piotr <piot.domzalski91@gmail.com>
2023-01-06 15:00:27 +0100

    add the path to the filter

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screens/tabs_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/filters_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,11 +32,13 @@ class MyApp extends StatelessWidget {
               )),
         ),
         // home: CategoriesScreen(),
-        initialRoute: '/', // default is '/',
+        initialRoute: '/',
+        // default is '/',
         routes: {
           '/': (ctx) => TabsScreen(),
           CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
           MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+          FiltersScreen.routeName: (ctx) => FiltersScreen(),
         },
         onGenerateRoute: (settings) {
           print(settings.arguments);
